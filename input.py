82be0e9d5eb49e0ad34d8a6714ea97204eac431e
unknown <cecce@FRCLOC203-67392.ecolecatholique.ca>
2020-12-04 09:44:04 -0500

    Je suis égaré

---
+++ input.py
@@ -0,0 +1,13 @@
+# Alioune Badara Seck input 
+# 02 Décembre 2020
+print('Quel âge avez vous?')
+âge=input()
+print("as-tu",âge,"?")
+print('oui ou non ou passer')
+if choix=='oui'
+print('réponse correct')
+elif choix=='non'
+print('faux, veuillez réessayer de nouveau')
+elif choix==('passer')
+print('dommage que vous ne je soyez pas entêté a trouver la réponse')
+print ("Merci d'avoir participer",name)
