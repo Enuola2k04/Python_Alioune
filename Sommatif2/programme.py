5580b5292e2d0799c487062be1a1aa623e8c381d
Enuola2k04 <74927723+Enuola2k04@users.noreply.github.com>
2020-12-06 10:25:38 -0500

    Update programme.py
    Terminé

--- Sommatif2/programme.py
+++ Sommatif2/programme.py
@@ -1,2 +1,35 @@
-# Programme intéractif a la consol
+# Programme intéractif a la console
 # Pour Dimanche 06 Décembre 2020
+#Nom:         Un quiz (Un quiz jeu.py)
+#But:         quiz
+#Auteur:      Alioune Badara Seck
+#Crée:        03-DEC-2020
+#Mis à jour:  06-DEC-2020
+nom = input('Quel est votre nom:')
+print('Bienvenue,'+ nom + '!')
+print("Vous venez d'entrer dans un petit quiz avec Alioune")
+print()
+print("Commençons")
+print("(PS: toute les réponses commencent avec une majuscule)")
+print("")
+Un = input("Quel est le Symbole de L'Hélium: ")
+Deux = input("Quel est la capitale du Canada: ")
+Trois = input("Quel est le nom de famille du prof d'info: ")
+Un1 = "He"
+Deux2 = "Ottawa"
+Trois3 = "Crowley"
+if Un==Un1:
+  print("1.correct")
+else:
+  print("1.Incorrect")
+if Deux==Deux2:
+  print("2.correct")
+else:
+  print("2.Incorrect")
+if Trois==Trois3:
+  print("3.correct")
+else:
+  print("3.Incorrect")
+print("")
+print("Merci"+" "+ nom +" "+ "pour votre temps.")
+Merci = input()
